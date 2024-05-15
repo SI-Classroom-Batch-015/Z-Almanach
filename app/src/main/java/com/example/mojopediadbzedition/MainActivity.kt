327bfc695fbe43397022315ddf8b6c664139328d
Michael Fleps <f.michi84.989@gmail.com>
2024-05-16 01:22:42 +0200

    MainActivity.kt Die BTMBar mit dem Controller des NavHostFragments verknüpft

--- app/src/main/java/com/example/mojopediadbzedition/MainActivity.kt
+++ app/src/main/java/com/example/mojopediadbzedition/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mojopediadbzedition
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.mojopediadbzedition.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +12,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Verknüpft die Bottom-Navigation-Bar mit dem Navigation Controller des NavHost-Fragments
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navHost.navController)
     }
 }
