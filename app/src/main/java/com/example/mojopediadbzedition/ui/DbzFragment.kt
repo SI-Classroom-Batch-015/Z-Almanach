db339f7c979704e700b72d46fae2c2cc7e0fb65f
Michael Fleps <f.michi84.989@gmail.com>
2024-05-21 12:54:17 +0200

    DbzFragment, Adapter Init und Konfig, Direction samt Safe ARgs gesetzt sowie Observer eingerichtet

--- app/src/main/java/com/example/mojopediadbzedition/ui/DbzFragment.kt
+++ app/src/main/java/com/example/mojopediadbzedition/ui/DbzFragment.kt
@@ -5,17 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.mojopediadbzedition.MainViewModel
+import com.example.mojopediadbzedition.adapter.DbzAdapter
 import com.example.mojopediadbzedition.databinding.FragmentDbzBinding
 
 class DbzFragment : Fragment() {
 
     private lateinit var binding: FragmentDbzBinding
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentDbzBinding.inflate(inflater, container, false)
+        viewModel.loadCharacters()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Adapter Init und Konfiguriert samt Safe Args
+        val adapter = DbzAdapter { character ->
+            findNavController()
+                .navigate(
+                    DbzFragmentDirections.actionDbzFragmentToDbzDetailFragment(
+                        character.characterImage,
+                        character.characterName,
+                        character.ki,
+                        character.maxKi,
+                        character.race,
+                        character.descriptionSpain,
+                    )
+                )
+        }
+        binding.rvDbzFragment.adapter = adapter
+        viewModel.characters.observe(viewLifecycleOwner) { adapter.submitList(it) }
     }
 }
