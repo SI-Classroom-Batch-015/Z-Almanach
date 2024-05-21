3b1aaabe50f6bf5fd814336f3d694cb783366539
Michael Fleps <f.michi84.989@gmail.com>
2024-05-21 12:37:05 +0200

    MainViewmodel LiveData zwecks Lebenszyklus überleben gesetzt

--- app/src/main/java/com/example/mojopediadbzedition/MainViewModel.kt
+++ app/src/main/java/com/example/mojopediadbzedition/MainViewModel.kt
@@ -1,26 +1,32 @@
 package com.example.mojopediadbzedition
 
 import android.app.Application
-import android.content.ContentValues.TAG
+import android.content.ContentValues
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import com.example.mojopediadbzedition.data.Repository
 import com.example.mojopediadbzedition.data.local.CharacterDatabase
+import com.example.mojopediadbzedition.data.model.Character
 import com.example.mojopediadbzedition.data.remote.DbzApi
 import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = Repository(DbzApi, CharacterDatabase.getDatabase(application))
+    // Instanz der lokalen Datenbank
+    private val database = CharacterDatabase.getDatabase(application)
+    // Instanz vom Repo, das die Datenbank- und API-Zugriffe übergeben kriegt
+    private val repository: Repository = Repository(DbzApi, database)
+
+    val characters: LiveData<List<Character>> = repository.characters
 
     fun loadCharacters() {
         viewModelScope.launch {
             try {
                 repository.getCharacters() // Charaktere aus dem Repository aktualisieren
             } catch (e: Exception) {
-                Log.e(TAG, "Fail Load Characters $e")
-            }
+                Log.e(ContentValues.TAG, "Fail Load Characters $e")            }
         }
     }
 }
