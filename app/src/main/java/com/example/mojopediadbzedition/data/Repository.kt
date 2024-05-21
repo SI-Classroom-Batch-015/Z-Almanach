5543e94d1c952ec251fe92a74743cd8146e14faf
Michael Fleps <f.michi84.989@gmail.com>
2024-05-21 03:21:28 +0200

    Repository zentrale Punkt für Datenoperationen. Läat Daten von der Api und speichert dies in die db

---
+++ app/src/main/java/com/example/mojopediadbzedition/data/Repository.kt
@@ -0,0 +1,38 @@
+package com.example.mojopediadbzedition.data
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import com.example.mojopediadbzedition.data.local.CharacterDatabase
+import com.example.mojopediadbzedition.data.remote.DbzApi
+import com.example.mojopediadbzedition.data.model.Character
+import com.example.mojopediadbzedition.data.model.Characters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class Repository(
+    private val api: DbzApi,
+    private val database: CharacterDatabase) {
+
+    private val _characters: LiveData<List<Character>> = database.characterDao.getAllCharacter()
+    val characters: LiveData<List<Character>>
+        get() = _characters
+
+    // Fun die asyncron im Hintergrund laufen kann, ohne die UI zu blockieren
+    suspend fun refreshCharacters() {
+        withContext(Dispatchers.IO) {
+            try {
+                val charactersResponse: Characters = api.retrofitService.getCharacters()
+
+                // Extrahieren der Liste von Charakteren aus der API-Antwort
+                val characterList: List<Character> = charactersResponse.listOfCharacters
+
+                database.characterDao.deleteAll()
+
+                // Einfügen der neuen Charaktere in die Datenbank
+                database.characterDao.insertCharacters(characterList)
+            } catch (e: Exception) {
+                Log.e("Repository", "Error Load characters: ${e.message}")
+            }
+        }
+    }
+}
