ea7e982e79d5383df3cd3ca9d3f308281a98b49b
Michael Fleps <f.michi84.989@gmail.com>
2024-05-21 12:56:16 +0200

    DbzDetailFragmente, auf die SafeArgs zugegriffen und sie in die Views geladen

--- app/src/main/java/com/example/mojopediadbzedition/ui/DbzDetailFragment.kt
+++ app/src/main/java/com/example/mojopediadbzedition/ui/DbzDetailFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import coil.load
 import com.example.mojopediadbzedition.databinding.FragmentDbzDetailBinding
 
 class DbzDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDbzDetailBinding
+    private val args: DbzDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +25,20 @@ class DbzDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val characterImage = args.image
+        val characterName = args.name
+        val ki = args.ki
+        val maxKi = args.maxKi
+        val race = args.race
+        val description = args.description
+
+        binding.apply {
+            ivCharacterImage.load(characterImage)
+            tvCharacterName.text = characterName
+            tvKi.text = ki
+            tvMaxKi.text = maxKi
+            tvRace.text = race
+            tvDescription.text = description
+        }
     }
 }
