6089994c57ec5fe61e83254e046fa9be26800776
Michael Fleps <f.michi84.989@gmail.com>
2024-05-23 02:36:11 +0200

    SCRUM BOARD mit Github verknüpft und aufgesetzt

--- app/src/main/java/com/example/zalmanach/ui/HomeFragment.kt
+++ app/src/main/java/com/example/zalmanach/ui/HomeFragment.kt
@@ -21,5 +21,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //
     }
 }
