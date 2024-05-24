f9545038f8b65adb8550fa389dd75543b2dc7464
Michael Fleps <f.michi84.989@gmail.com>
2024-05-24 10:31:04 +0200

    MainActivity, Callback aktiviert und A-Studio Shadow Bug aufgelöst

--- app/src/main/java/com/example/zalmanach/MainActivity.kt
+++ app/src/main/java/com/example/zalmanach/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.zalmanach
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.zalmanach.databinding.ActivityMainBinding
@@ -16,9 +18,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Android Studio SHADOW-BUG MaterialBottomBar aufgelöst
+        binding.bottomNavigationView.background = null
+
         // Verknüpft die Bottom-Navigation-Bar mit dem Navigation Controller des NavHost-Fragments
         val navHost =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         binding.bottomNavigationView.setupWithNavController(navHost.navController)
+
+        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                binding.fragmentContainerView.findNavController().navigateUp()
+            }
+        })
     }
 }
