49df8ab1fa7c2aca2fd086f7c90180e8456c7b94
Michael Fleps <f.michi84.989@gmail.com>
2024-05-26 23:52:27 +0200

    Datenklasse für Transformationen samst Entity für db erstellt

---
+++ app/src/main/java/com/example/zalmanach/data/model/Transformation.kt
@@ -0,0 +1,19 @@
+package com.example.zalmanach.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
+
+@Entity("transformation_table")
+data class Transformation(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    @Json(name = "name")
+    val transformationName: String,
+
+    @Json(name = "image")
+    val transformationImage: String,
+
+    val transformationKi: String,
+)
