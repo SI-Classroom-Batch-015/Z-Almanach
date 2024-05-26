c3eb286fa76b18049be840db679f0619b069123f
Michael Fleps <f.michi84.989@gmail.com>
2024-05-27 00:19:24 +0200

    Database um die Transformation Entity erweitert und auf Version 2 gesetzt

--- app/src/main/java/com/example/zalmanach/data/local/DragonballDatabase.kt
+++ app/src/main/java/com/example/zalmanach/data/local/DragonballDatabase.kt
@@ -5,8 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.zalmanach.data.model.Character
+import com.example.zalmanach.data.model.Transformation
 
-@Database(entities = [Character::class], version = 1)
+@Database(entities = [Character::class, Transformation::class], version = 2)
 abstract class DragonballDatabase : RoomDatabase() {
 
     // DAo Deklarieren
