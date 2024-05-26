d2d77e5123f2fc932786dfb15cf1ba46f1a6602a
Michael Fleps <f.michi84.989@gmail.com>
2024-05-27 00:38:39 +0200

    Repository, die Transformationen aus der API geladen und in die Datenbank einfügen

--- app/src/main/java/com/example/zalmanach/data/Repository.kt
+++ app/src/main/java/com/example/zalmanach/data/Repository.kt
@@ -6,6 +6,7 @@ import com.example.zalmanach.data.local.DragonballDatabase
 import com.example.zalmanach.data.remote.DbzApi
 import com.example.zalmanach.data.model.Character
 import com.example.zalmanach.data.model.Characters
+import com.example.zalmanach.data.model.Transformation
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -18,21 +19,32 @@ class Repository(
     val characters: LiveData<List<Character>>
         get() = _characters
 
+    private val _transformations: LiveData<List<Transformation>> = database.dragonballDao.getAllTransformations()
+    val transformations: LiveData<List<Transformation>>
+        get() = _transformations
+
     // Fun die asyncron im Hintergrund laufen kann, ohne die UI zu blockieren
     suspend fun getCharacters() {
         withContext(Dispatchers.IO) {
             try {
                 val charactersResponse: Characters = api.retrofitService.getCharacters()
-
-                // Extrahieren der Liste von Charakteren aus der API-Antwort
                 val characterList: List<Character> = charactersResponse.listOfCharacters
-
                 database.dragonballDao.deleteAll()
-
-                // Einfügen der neuen Charaktere in die Datenbank
                 database.dragonballDao.insertCharacters(characterList)
             } catch (e: Exception) {
                 Log.e("Repository", "Error Load characters: ${e.message}")
+            }
+        }
+    }
+
+    suspend fun getTransformations() {
+        withContext(Dispatchers.IO) {
+            try {
+                val transformationList: List<Transformation> = api.retrofitService.getTransformations()
+                database.dragonballDao.deleteAllTransformations()
+                database.dragonballDao.insertTransformations(transformationList)
+            } catch (e: Exception) {
+                Log.e("Reposirotry", "Error Load Transformations: ${e.message}")
             }
         }
     }
