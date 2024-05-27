555a7f73cca3997b3ba6565d92b4effcc2df00da
Michael Fleps <f.michi84.989@gmail.com>
2024-05-27 05:16:37 +0200

    Homefragment, RV für Planeten konfiguruert

--- app/src/main/java/com/example/zalmanach/ui/HomeFragment.kt
+++ app/src/main/java/com/example/zalmanach/ui/HomeFragment.kt
@@ -7,9 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.zalmanach.MainViewModel
+import com.example.zalmanach.adapter.PlanetAdapter
 import com.example.zalmanach.adapter.TransformationAdapter
 import com.example.zalmanach.databinding.FragmentHomeBinding
 import com.example.zalmanach.utils.AnimationUtils
@@ -25,6 +25,7 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         viewModel.loadTransformations()
+        viewModel.loadPlanets()
         return binding.root
     }
 
@@ -33,23 +34,34 @@ class HomeFragment : Fragment() {
 
         // Löst EsterEgg aus
         binding.ivZ.setOnClickListener { viewModel.triggerAnimation() }
-        viewModel.startAnimation.observe(viewLifecycleOwner, Observer { isRotate ->
-        if (isRotate) {
-            AnimationUtils.startZAnimation(binding.ivZ)
+        viewModel.startAnimation.observe(viewLifecycleOwner) { isRotate ->
+            if (isRotate) {
+                AnimationUtils.startZAnimation(binding.ivZ)
+            }
         }
-        })
 
         // LinearLayoutManager erstellen und RV zuweisen
-        val layoutManager = LinearLayoutManager(requireContext())
-        binding.rvHomeTransformations.layoutManager = layoutManager
+        val layoutManagerVertical = LinearLayoutManager(requireContext())
+        binding.rvHomeTransformations.layoutManager = layoutManagerVertical
 
-        val adapter = TransformationAdapter { transformation ->
-            Toast.makeText(requireContext(), "Transformation", Toast.LENGTH_LONG).show()
+        // Adapter Objekt erstellt / init und mit dem Lamdaa bei Klick Konfi
+        val transformationAdapter = TransformationAdapter { transformation ->
+            Toast.makeText(requireContext(), "Transformations", Toast.LENGTH_LONG).show()
         }
-        binding.rvHomeTransformations.adapter = adapter
+        binding.rvHomeTransformations.adapter = transformationAdapter
 
         viewModel.transformations.observe(viewLifecycleOwner) {
-            adapter.submitList(it)
+            transformationAdapter.submitList(it)
+        }
+
+        val layoutManagerHorizontal = LinearLayoutManager(requireContext())
+        binding.rvHomePlanets.layoutManager = layoutManagerHorizontal
+        val planetAdapter = PlanetAdapter { planet ->
+            Toast.makeText(requireContext(), "Planets", Toast.LENGTH_LONG).show()
+        }
+        binding.rvHomePlanets.adapter = planetAdapter
+        viewModel.planets.observe(viewLifecycleOwner) {
+            planetAdapter.submitList(it)
         }
     }
 }
