af616a815948f024b360b4bfb5eeb1ede41db233
Michael Fleps <f.michi84.989@gmail.com>
2024-05-28 16:43:15 +0200

    SearchF., Sucheingaben beoabachten -> onQuery-Textwatcher override fun gesetzt, mittels private fun und obeserver

--- app/src/main/java/com/example/zalmanach/ui/SearchFragment.kt
+++ app/src/main/java/com/example/zalmanach/ui/SearchFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.example.zalmanach.MainViewModel
+import com.example.zalmanach.adapter.SearchAdapter
 import com.example.zalmanach.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(), SearchView.OnQueryTextListener {
 
     private lateinit var binding: FragmentSearchBinding
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +23,45 @@ class SearchFragment : Fragment() {
     ): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Adapter init. und RV hinzufügen
+        val adapter = SearchAdapter(emptyList())
+        binding.rvSearchResult.adapter = adapter
+
+        binding.textInput.setOnQueryTextListener(this)
+
+        binding.rvSearchResult.adapter = adapter
+        binding.rvSearchResult.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+       newText?.let {
+           search(it)
+       }
+        return true
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        query?.let {
+            search(it)
+        }
+        return true
+    }
+    private fun search(query: String) {
+        viewModel.searchCharacters(query).observe(viewLifecycleOwner) {
+            it?.let {
+                (binding.rvSearchResult.adapter as SearchAdapter).submitList(it)
+            }
+        }
     }
 }
