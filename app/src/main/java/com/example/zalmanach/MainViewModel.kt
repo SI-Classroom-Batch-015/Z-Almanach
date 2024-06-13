a005542e8fef63ab9aa2cc1837397282c9df8bcb
Michael Fleps <f.michi84.989@gmail.com>
2024-06-13 08:54:50 +0200

    MutLD für die jeweiligen ausgew. Argumente gesetzt und fun implementiert die die aktuel.ausg. Arg. setzt

--- app/src/main/java/com/example/zalmanach/MainViewModel.kt
+++ app/src/main/java/com/example/zalmanach/MainViewModel.kt
@@ -19,6 +19,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     // Instanz der lokalen Datenbank
     private val database = DragonballDatabase.getDatabase(application)
+
     // Instanz vom Repo, das die Datenbank- und API-Zugriffe verwaltet
     private val repository: Repository = Repository(DbzApi, database)
 
@@ -29,20 +30,38 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     // MutableLiveData für die Animation
     private val _startAnimation = MutableLiveData<Boolean>()
-    val startAnimation: LiveData<Boolean> get() = _startAnimation
+    val startAnimation: LiveData<Boolean>
+        get() = _startAnimation
 
-    // Methode zum Triggern der Animation
+    // Triggern der Animation
     fun triggerAnimation() {
         _startAnimation.value = true // Startet die Ani
     }
 
-    // Methode um Daten zu laden, werden in Fragmenten aufgerufen
+    // MutableLiveData für das ausgewählte Charakterbild
+    private val _selectedCharacterImage = MutableLiveData<String>()
+    val selectedCharacterImage: LiveData<String>
+        get() = _selectedCharacterImage
+
+    // MutableLiveData für den ausgewählten Charakternamen
+    private val _selectedCharacterName = MutableLiveData<String>()
+    val selectedCharacterName: LiveData<String>
+        get() = _selectedCharacterName
+
+    // Setzen des ausgewählten Charakters
+    fun setSelectedCharacter(characterImage: String, characterName: String) {
+        _selectedCharacterImage.value = characterImage
+        _selectedCharacterName.value = characterName
+    }
+
+    // Daten zu laden, werden in Fragmenten aufgerufen
     fun loadCharacters() {
         viewModelScope.launch {
             try {
                 repository.getCharacters() // Charaktere aus dem Repository holen
             } catch (e: Exception) {
-                Log.e("MainViewModel", "Fail Load Characters ${e.message}")}
+                Log.e("MainViewModel", "Fail Load Characters ${e.message}")
+            }
         }
     }
 
@@ -61,8 +80,46 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             try {
                 repository.getPlanets()
             } catch (e: Exception) {
-                Log.e("MainViewModel", "Fail Load Planets ${e.message}")}
+                Log.e("MainViewModel", "Fail Load Planets ${e.message}")
+            }
         }
+    }
+
+//    fun searchCharacterByName(characterName: String): LiveData<Character?> {
+//        val resultCharacterByName = MutableLiveData<Character?>()               // Init M.LiveData
+//        viewModelScope.launch {
+//            try {
+//                val character = repository.searchCharacterByName(characterName)
+//                resultCharacterByName.postValue(character)
+//            } catch (e: Exception) {
+//                Log.e("MainViewModel", "Fail Search Character by Name ${e.message}")
+//            }
+//            resultCharacterByName.postValue(null)
+//        }
+//        return resultCharacterByName
+//    }
+
+    //    fun loadCharacterById(characterId: Int) {
+//        viewModelScope.launch {
+//            try {
+//                val characterById = repository.searchCharacterById(characterId)
+//                _selectedCharacter.postValue(characterById)
+//            } catch (e: Exception) {
+//                Log.e("MainViewModel", "Fail Load Character by Id ${e.message}")}
+//        }
+//    }
+
+    // Suchen von Charakteren
+    private fun searchByCharacters(query: String): LiveData<List<Character>> {
+        return repository.searchCharacters(query)
+    }
+
+    private fun searchByTransformations(query: String): LiveData<List<Transformation>> {
+        return repository.searchTransformations(query)
+    }
+
+    private fun searchByPlanets(query: String): LiveData<List<Planet>> {
+        return repository.searchPlanets(query)
     }
 
     // "searchByAll" wird vom Benuzter im Fragment ausgelöst. MediatorLiveData, kann mehrere andere LiveData-Quellen überwachen
@@ -95,20 +152,6 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                 results.value = combinedResults
             }
         }
-
         return results
-    }
-
-    // Methode zum Suchen von Charakteren
-    private fun searchByCharacters(query: String) : LiveData<List<Character>> {
-        return repository.searchCharacters(query)
-    }
-
-    private fun searchByTransformations(query: String) : LiveData<List<Transformation>> {
-        return repository.searchTransformations(query)
-    }
-
-    private fun searchByPlanets(query: String) : LiveData<List<Planet>> {
-        return repository.searchPlanets(query)
     }
 }
