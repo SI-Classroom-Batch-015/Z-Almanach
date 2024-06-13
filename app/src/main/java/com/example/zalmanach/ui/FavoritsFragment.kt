9de4ec52a14be88d3c4f68debb04f7351bd3068b
Michael Fleps <f.michi84.989@gmail.com>
2024-06-13 17:43:33 +0200

    TL-> noch kleiner Layout Bug. Query-Filter gesetz und fun die durch die Kombi.Liste it. und die Daten ans tl in die TableRows bindet

--- app/src/main/java/com/example/zalmanach/ui/FavoritsFragment.kt
+++ app/src/main/java/com/example/zalmanach/ui/FavoritsFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableRow
 import androidx.fragment.app.activityViewModels
 import coil.load
 import com.example.zalmanach.MainActivity
 import com.example.zalmanach.MainViewModel
+import com.example.zalmanach.data.model.Character
+import com.example.zalmanach.data.model.Transformation
 import com.example.zalmanach.databinding.FragmentFavoritsBinding
+import com.example.zalmanach.databinding.ListItemFavoritVilliansBinding
 
 class FavoritsFragment : Fragment() {
 
@@ -30,13 +34,57 @@ class FavoritsFragment : Fragment() {
         // Bottom Nav ausblenden, Fun in der Main
         (activity as MainActivity).showBottomNav(false)
 
-        // Ausgewählten Character beobachten und die Daten an die UI zu binden
+        // Daten beobachten und an die UI zu binden
         viewModel.selectedCharacterImage.observe(viewLifecycleOwner) { characterImage ->
             binding.ivFavoriteCharacter.load(characterImage)
         }
 
         viewModel.selectedCharacterName.observe(viewLifecycleOwner) { characterName ->
             binding.tvFavoriteName.text = characterName
+        }
+
+        // Kombinierte Gegner-Liste aktualisieren und Query-Filter mitgeben
+        viewModel.getCombinedVillains("male")
+        viewModel.villains.observe(viewLifecycleOwner) { villains ->
+            updateVillainsTable(villains)
+        }
+    }
+
+    private fun updateVillainsTable(villains: List<Any>) {
+
+        // Zuerst alle Entfernen und wieder Neu setzen
+        binding.tlFavoriteVillians.removeAllViews()
+
+        villains.forEach { villain ->
+            val tableRow = TableRow(context)
+            val itemViewBinding = ListItemFavoritVilliansBinding.inflate(layoutInflater, tableRow, false)
+
+            when (villain) {
+                is Character -> {
+                    itemViewBinding.ivFavoriteVillians.load(villain.characterImage) {
+                        listener(
+                            onError = { _, _ ->
+                                itemViewBinding.tvFavoriteGoneName.visibility = View.VISIBLE
+                            }
+                        )
+                    }
+                    itemViewBinding.tvFavoriteGoneName.text = villain.characterName
+                }
+                is Transformation -> {
+                    itemViewBinding.ivFavoriteVillians.load(villain.transformationImage) {
+                        listener(
+                            onError = { _, _ ->
+                                itemViewBinding.tvFavoriteGoneName.visibility = View.VISIBLE
+                            }
+                        )
+                    }
+                    itemViewBinding.tvFavoriteGoneName.text = villain.transformationName
+                }
+            }
+
+            // Instanz vom itemViewBinding zur TableRow; danach zum TableLayout hinzufügen
+            tableRow.addView(itemViewBinding.root)
+            binding.tlFavoriteVillians.addView(tableRow)
         }
     }
 
