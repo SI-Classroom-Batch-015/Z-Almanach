68227d77b1b80e8b5cb4749964342ff21b98b236
Michael Fleps <f.michi84.989@gmail.com>
2024-06-16 20:55:32 +0200

    REPLACE und IGNORE aufgeteilt... SQL-db-Op-Befehle gesetzt, Variablen erneut expliziter umbenannt

--- app/src/main/java/com/example/zalmanach/data/model/FavoriteItem.kt
+++ app/src/main/java/com/example/zalmanach/data/model/FavoriteItem.kt
@@ -7,8 +7,8 @@ import androidx.room.PrimaryKey
 data class FavoriteItem(
     @PrimaryKey(autoGenerate = true)
 
-    val itemId: Int = 0,
-    val itemType: String,
-    val itemName: String,
-    val itemImage: String,
+    val itemFavoriteId: Int = 0,
+    val itemFavoriteType: String,
+    val itemFavoriteName: String,
+    val itemFavoriteImage: String,
 )
