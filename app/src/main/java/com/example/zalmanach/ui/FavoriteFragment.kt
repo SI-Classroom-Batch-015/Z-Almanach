d5c37093a57473378a48d229c705ac0294b97014
Michael Fleps <f.michi84.989@gmail.com>
2024-06-18 15:00:09 +0200

    Rv mithilfe von FavoriteAdapter gesetzt und mit submitlist die Ergebinsse Aktuel gehalten

--- app/src/main/java/com/example/zalmanach/ui/FavoriteFragment.kt
+++ app/src/main/java/com/example/zalmanach/ui/FavoriteFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.example.zalmanach.MainViewModel
+import com.example.zalmanach.adapter.FavoriteAdapter
 import com.example.zalmanach.databinding.FragmentFavoriteBinding
 
 class FavoriteFragment : Fragment() {
@@ -25,6 +28,13 @@ class FavoriteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TODO
+        val adapter = FavoriteAdapter { favorite ->
+      //      viewModel.removeFromFavorite(favorite.favoriteId, favorite.favoriteType)
+            Toast.makeText(requireContext(), "Aus Favoriten Entfernt", Toast.LENGTH_SHORT).show()
+        }
+        binding.rvFavorite.adapter = adapter
+        viewModel.favorite.observe(viewLifecycleOwner, Observer { favorites ->
+            adapter.submitList(favorites)
+        })
     }
 }
